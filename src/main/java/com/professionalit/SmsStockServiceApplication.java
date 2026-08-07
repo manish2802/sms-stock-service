4e7d0673764339e227fd040f5e28ecc74905a3cb
manish2802 <manishverma.java@gmail.com>
2026-08-08 01:49:24 +0530

    added configuration

--- src/main/java/com/professionalit/SmsStockServiceApplication.java
+++ src/main/java/com/professionalit/SmsStockServiceApplication.java
@@ -2,11 +2,10 @@ package com.professionalit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableScheduling
 public class SmsStockServiceApplication {
 	public static void main(String[] args) {
